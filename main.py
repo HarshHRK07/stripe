8befc782cf0c72fa2d6ec9bb8221fc532ac19573
HarshHRK07 <149139740+HarshHRK07@users.noreply.github.com>
2024-04-02 10:44:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,5 +84,5 @@ def generate_token_ignore_cvc():
         return jsonify({"error": "Missing 'cc' or 'pk' parameter"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
     
